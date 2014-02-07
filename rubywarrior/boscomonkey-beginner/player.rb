af30b03af137351f34868094d49c5a77a413ea86
Bosco So <bosco@indiegogo.com>
2014-02-06 19:44:41 -0800

    level 3 done

--- rubywarrior/boscomonkey-beginner/player.rb
+++ rubywarrior/boscomonkey-beginner/player.rb
@@ -1,9 +1,17 @@
 class Player
   def play_turn(warrior)
     if warrior.feel.empty?
-      warrior.walk!
+      if should_rest(warrior)
+        warrior.rest!
+      else
+        warrior.walk!
+      end
     else
       warrior.attack!
     end
+  end
+
+  def should_rest(warrior)
+    warrior.health < 18
   end
 end
