05239cb0f1d74b5afab1549c065dc576900d0a58
Bosco So <bosco@sfruby.info>
2014-02-13 00:09:10 -0800

    using hobo rules engine to resolve if-then-else rules

--- rubywarrior/boscomonkey-beginner/player.rb
+++ rubywarrior/boscomonkey-beginner/player.rb
@@ -10,41 +10,96 @@ class Player
     @warrior       = nil
   end
 
+=begin
+
+if enemy_immediately_in_direction
+  attack!(direction)
+
+if enemy_in_arrow_range_on_both_sides
+  if last_attack_direction
+    reverse_last_attack_direction
+  else
+    randomly_choose_attack_direction
+  end
+  shoot!(last_attack_direction)
+else
+  cancel_last_attack_direction
+
+if captive_immediately_in_direction
+  rescue!(direction)
+
+if feel_wall?
+  reverse_direction!
+
+if feel_captive?
+  rescue!
+
+if empty?
+  walk!
+
+=end
   def play_turn(avatar)
     remember_current_warrior(avatar)
 
-    puts "#{scan(:backward).reverse}\tMYSELF\t#{scan(:forward)}"
+    # puts "#{scan(:backward).reverse}\tMYSELF\t#{scan(:forward)}"
 
     random_direction = (rand(2) == 0 ? :forward : :backward)
     other_direction  = (random_direction == :forward ? :backward : :forward)
 
-    if target_in_sight?(random_direction)
-      shoot!(random_direction)
-    elsif target_in_sight?(other_direction)
-        shoot!(other_direction)
-    elsif taking_damage?
-      signal_retreat
-      advance
-    elsif resting? || should_rest?
-      rest!
-    else
-      cancel_retreat if retreating?
-      advance
-    end
+    Rules.new.
+      add(:target_in_sight_random_direction,
+          lambda {|r| target_in_sight?(random_direction) },
+          lambda {|r|
+            shoot!(random_direction)
+            r.stop! }
+          ).
+      add(:target_in_sight_other_direction,
+          lambda {|r| target_in_sight?(other_direction) },
+          lambda {|r|
+            shoot!(other_direction)
+            r.stop! }
+          ).
+      add(:captive_in_sight_random_direction,
+          lambda {|r| captive_in_sight?(random_direction) },
+          lambda {|r| @direction = random_direction }
+          ).
+      add(:captive_in_sight_other_direction,
+          lambda {|r| captive_in_sight?(other_direction) },
+          lambda {|r| @direction = other_direction }
+          ).
+      add(:resting_or_should_rest,
+          lambda {|r| resting? || should_rest? },
+          lambda {|r|
+            rest!
+            r.stop! }
+          ).
+      add(:attack_if_enemy,
+          lambda {|r| feel_enemy? },
+          lambda {|r|
+            attack!
+            r.stop! }
+          ).
+      add(:pivot_if_wall,
+          lambda {|r| feel_wall? },
+          lambda {|r|
+            reverse_direction!
+            r.stop! }
+          ).
+      add(:rescue_if_captive,
+          lambda {|r| feel_captive? },
+          lambda {|r|
+            rescue_captive!
+            r.stop! }
+          ).
+      add(:walk_if_empty,
+          lambda {|r| feel_empty? },
+          lambda {|r|
+            walk!
+            r.stop! }
+          ).
+      run
 
     remember_health
-  end
-
-  def advance
-    if feel_captive?
-      rescue_captive!
-    elsif feel_empty?
-      walk!
-    elsif feel_wall?
-      reverse_direction
-    else
-      attack!
-    end
   end
 
   def attack!
@@ -56,12 +111,30 @@ class Player
     @direction = :forward
   end
 
+  def captive_in_sight?(orientation=self.direction)
+    look(orientation).each do |cell|
+      if cell.captive?
+        return true
+      elsif cell.wall? || cell.enemy?
+        return false
+      elsif cell.empty?
+        # do nothing and loop again
+      end
+    end
+
+    false
+  end
+
   def feel_captive?
     warrior.feel(direction).captive?
   end
 
   def feel_empty?
     warrior.feel(direction).empty?
+  end
+
+  def feel_enemy?
+    warrior.feel(direction).enemy?
   end
 
   def feel_wall?
@@ -86,7 +159,7 @@ class Player
 
   def rest!
     warrior.rest!
-    @on_rest = (warrior.health < @max_health-2)
+    @on_rest = (warrior.health < @max_health/2)
   end
 
   def resting?
@@ -97,7 +170,7 @@ class Player
     @on_retreat
   end
 
-  def reverse_direction
+  def reverse_direction!
     warrior.pivot!(:backward)
   end
 
@@ -158,5 +231,50 @@ class Player
         memo.push(enemy.join)
       end
     }
+  end
+end
+
+
+class Rules
+  def initialize
+    @rules = []
+    @exit_run = false
+  end
+
+  def add(name, if_proc, then_proc, else_proc=nil)
+    raise "'name' cannot be nil or empty" if (name.nil? || name.empty?)
+
+    rule_body = {name: name, if: if_proc, then: then_proc, else: else_proc}
+    @rules.push rule_body
+
+    self	# for chaining
+  end
+
+  def log_rule(success, name)
+    puts "RULE:\t#{success ? '+' : '-'}\t#{name}" if success
+  end
+
+  def run
+    @exit_run = false
+
+    @rules.each do |rule|
+      if rule[:if].call(self)
+        rule[:then].call(self)
+
+        log_rule(true, rule[:name])
+      else
+        if rule[:else]
+          rule[:else].call(self)
+        end
+
+        log_rule(false, rule[:name])
+      end
+
+      break if @exit_run
+    end
+  end
+
+  def stop!
+    @exit_run = true
   end
 end
